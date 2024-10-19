7bc8656fdaf6e5bf80e8bf2e54bc54feaf64ec73
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-10-19 16:13:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {useEffect, useState} from "react";
 
 function App() {
 
@@ -14,32 +13,9 @@ function App() {
     *
     * */
 
-    const [quoteResponses, setQuoteResponses] = useState([]);
-    useEffect(() => {
-        const category = "happiness";
-        fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`,{
-            headers: { 'X-Api-Key': 'lMR7MvBsNn9GjF8solFxRA==i1Eb7mTNQckR5Lnv  '}
-        }).then(res => {
-            return res.json();
-        }).then(res => {
-            console.log(res);
-            setQuoteResponses(res);
-        })
-    }, []);
-
     return (
         <div className="App">
-            {quoteResponses.map(quoteResponse => <div>
-                <div>
-                    {quoteResponse.quote}
-                </div>
-                <div>
-                    {quoteResponse.author}
-                </div>
-                <div>
-                    {quoteResponse.category}
-                </div>
-            </div>)}
+          
         </div>
     );
 }
