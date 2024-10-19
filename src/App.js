eeeff35e11bc8d778181c3dbe5ab6622580b4b96
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-10-19 16:12:26 -0700

    connect to quote api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    /* sample quote response
+    * [
+  {
+    "quote": "The will of man is his happiness.",
+    "author": "Friedrich Schiller",
+    "category": "happiness"
+  }
+]
+    *
+    * */
+
+    const [quoteResponses, setQuoteResponses] = useState([]);
+    useEffect(() => {
+        const category = "happiness";
+        fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`,{
+            headers: { 'X-Api-Key': 'lMR7MvBsNn9GjF8solFxRA==i1Eb7mTNQckR5Lnv  '}
+        }).then(res => {
+            return res.json();
+        }).then(res => {
+            console.log(res);
+            setQuoteResponses(res);
+        })
+    }, []);
+
+    return (
+        <div className="App">
+            {quoteResponses.map(quoteResponse => <div>
+                <div>
+                    {quoteResponse.quote}
+                </div>
+                <div>
+                    {quoteResponse.author}
+                </div>
+                <div>
+                    {quoteResponse.category}
+                </div>
+            </div>)}
+        </div>
+    );
 }
 
 export default App;
